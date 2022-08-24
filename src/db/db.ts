585f4421b77bbda37acef97b4b02df6a8ebc6712
Saajan Shrestha <shresthasaajan31@gmail.com>
2022-08-24 10:55:17 +0545

    Update: Fix knex config

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,6 +4,6 @@ import knexConfig from "./knexfile";
 //@ts-ignore
 import knexStringCase from "knex-stringcase";
 
-const db = Knex(knexStringCase(knexConfig.development));
+const db = Knex(knexStringCase(knexConfig.production));
 
 export default db;
